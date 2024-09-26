f939f98194861403ca0e8da2c1c94d908e32cc3f
Imran <rimran84@yahoo.com>
2024-09-25 22:13:14 -0500

    added netconf xmltodic script

--- 4_Netcong_XML-to-Dict_Running_Config.py
+++ 4_Netcong_XML-to-Dict_Running_Config.py
@@ -19,16 +19,41 @@ NETCONG provide data in XML format.
 Below prohram will only data modeled in native YANG model 
 NETCONF provide data in XML format, We can convert this XML data into Python dictionary and
 store/embend in host inventory file like below, we will also inspect host key with ipdb
-"""
 
+Assumtion: Please make sure you are running basic OSFP on devices because code will pull OSPF Process ID
+"""
+"""
+IPDB inspection of routing protocols, in our example only OSPF configured
+
+ipdb> pp nr.inventory.hosts['CSR-R1']['run_cfg']
+{'data': {'@xmlns': 'urn:ietf:params:xml:ns:netconf:base:1.0',
+          '@xmlns:nc': 'urn:ietf:params:xml:ns:netconf:base:1.0',
+          'native': {'@xmlns': 'http://cisco.com/ns/yang/Cisco-IOS-XE-native',
+                     'router': {'ospf': {'@xmlns': 'http://cisco.com/ns/yang/Cisco-IOS-XE-ospf',
+                                         'auto-cost': {'reference-bandwidth': '100'},
+                                         'compatible': {'rfc1583': None},
+                                         'id': '127',
+                                         'timers': {'throttle': {'spf': {'delay': '50',
+                                                                         'max-delay': '5000',
+                                                                         'min-delay': '200'}}}},
+                                'router-ospf': {'@xmlns': 'http://cisco.com/ns/yang/Cisco-IOS-XE-ospf',
+                                                'ospf': {'process-id': {'id': '127',
+                                                                        'network': {'area': '12',
+                                                                                    'ip': '0.0.0.0',
+                                                                                    'wildcard': '255.255.255.255'}}}}}}}}
+ipdb> 
+
+"""
 def netconf_running_config (task):
     running_config = task.run (task=netconf_get_config, 
                                source="running", 
-                               path="/native", 
+                               path="/native/router", 
                                filter_type="xpath")
-    #xmltodict.parse(running_config.result)
+    task.host['run_cfg'] = xmltodict.parse(running_config.result.rpc.data_xml)
+    ospf_proc_id = task.host['run_cfg']['data']['native']['router']['ospf']['id']
+    print (f"[blue]{task.host}[/blue] running OSPF Process ID [red]*** {ospf_proc_id} ***[/red]")
         
-netconf_running_config_results = nr.run (task=netconf_running_config)
-print_result (netconf_running_config_results)
+nr.run (task=netconf_running_config)
 
 #ipdb.set_trace ()
+
